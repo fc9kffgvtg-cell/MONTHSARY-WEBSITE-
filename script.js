9ca3b67273dfb4d455e4a71d2046dd1ee0f6b838
fc9kffgvtg-cell <fc9kffgvtg@privaterelay.appleid.com>
2026-08-21 18:56:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function nextPage() {
-  window.location.href =
-"page2.html";
+    window.location.href = "page2.html";
+}
+
+function nextPage3() {
+    window.location.href = "page3.html";
+}
+
+function nextPage4() {
+    window.location.href = "page4.html";
+}
+
+function backToHome() {
+    window.location.href = "index.html";
 }
